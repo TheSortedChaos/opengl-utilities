1c35d4e36fc19ae139b25f749475fbd12d2b5d8a
Sorted Chaos <the.sorted.chaos@gmail.com>
2021-01-23 20:50:21 +0100

    #5 Add VAO function
    Also handle the cleanup of VAO IDs

--- src/main/scala/de/sorted/chaos/opengl/utilities/service/CleanUp.scala
+++ src/main/scala/de/sorted/chaos/opengl/utilities/service/CleanUp.scala
@@ -1,6 +1,7 @@
 package de.sorted.chaos.opengl.utilities.service
 
 import org.lwjgl.opengl.GL15.glDeleteBuffers
+import org.lwjgl.opengl.GL30.glDeleteVertexArrays
 import org.slf4j.LoggerFactory
 
 /**
@@ -10,6 +11,7 @@ object CleanUp {
   private val Log = LoggerFactory.getLogger(this.getClass)
 
   private var vertexBufferObjectIds = Vector.empty[Int]
+  private var vertexArrayObjectIds  = Vector.empty[Int]
 
   /**
     * Adds a VBO ID to the cleanup service
@@ -18,12 +20,20 @@ object CleanUp {
   def addVertexBufferObjectId(id: Int): Unit = vertexBufferObjectIds = vertexBufferObjectIds :+ id
 
   /**
+    * Adds a VAO id to the cleanup service
+    * @param id the ID of the VAO
+    */
+  def addVertexArrayObjectId(id: Int): Unit = vertexArrayObjectIds = vertexArrayObjectIds :+ id
+
+  /**
     * Starts the cleanup
     */
   def apply(): Unit = {
     Log.info("Start CleanUp...")
     Log.debug("  Remove VBO IDs")
     vertexBufferObjectIds.foreach(id => glDeleteBuffers(id))
+    Log.debug("  Remove VAO IDs")
+    vertexArrayObjectIds.foreach(id => glDeleteVertexArrays(id))
     Log.info("CleanUp done.")
   }
 }
