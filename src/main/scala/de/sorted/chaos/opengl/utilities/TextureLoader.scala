98a3d8d6d047e3982ab2401faedb377f3af3f0a1
Sorted Chaos <the.sorted.chaos@gmail.com>
2021-01-24 19:06:08 +0100

    #10 Add function to load a Texture

---
+++ src/main/scala/de/sorted/chaos/opengl/utilities/TextureLoader.scala
@@ -0,0 +1,56 @@
+package de.sorted.chaos.opengl.utilities
+
+import de.sorted.chaos.opengl.utilities.service.CleanUp
+import org.lwjgl.opengl.GL11._
+import org.lwjgl.opengl.GL30.glGenerateMipmap
+import org.lwjgl.stb.STBImage
+import org.lwjgl.system.MemoryStack
+
+import java.io.File
+
+object TextureLoader {
+  def load(filename: String): Int = {
+    val stack         = MemoryStack.stackPush()
+    val widthBuffer   = stack.mallocInt(1)
+    val heightBuffer  = stack.mallocInt(1)
+    val channelBuffer = stack.mallocInt(1)
+
+    val file = getClass.getResource(filename).getFile
+    // TODO this perhaps will create a problem, because the texture file is inside the resource folder inside the jar
+    val textureFile = new File(file).getAbsolutePath
+    STBImage.stbi_set_flip_vertically_on_load(true)
+    val buffer = STBImage.stbi_load(
+      textureFile,
+      widthBuffer,
+      heightBuffer,
+      channelBuffer,
+      4
+    )
+    if (buffer == null) {
+      throw new IllegalArgumentException(s"Image file '$textureFile' not loaded: ${STBImage.stbi_failure_reason()}")
+    }
+
+    val width  = widthBuffer.get()
+    val height = heightBuffer.get()
+
+    val textureId = glGenTextures()
+    glBindTexture(GL_TEXTURE_2D, textureId)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    glTexImage2D(
+      GL_TEXTURE_2D,
+      0,
+      GL_RGBA,
+      width,
+      height,
+      0,
+      GL_RGBA,
+      GL_UNSIGNED_BYTE,
+      buffer
+    )
+    glGenerateMipmap(GL_TEXTURE_2D)
+    STBImage.stbi_image_free(buffer)
+    CleanUp.addTextureId(textureId)
+
+    textureId
+  }
+}
